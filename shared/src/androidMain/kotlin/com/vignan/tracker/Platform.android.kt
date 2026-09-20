312cad45f0794838d84fe2a43f9a7dc237bb87ff
naveenxd <naveenxd@devh.in>
2026-09-20 17:31:47 +0530

    Add TimeInfo clock helpers for current period awareness

--- shared/src/androidMain/kotlin/com/vignan/tracker/Platform.android.kt
+++ shared/src/androidMain/kotlin/com/vignan/tracker/Platform.android.kt
@@ -1,6 +1,7 @@
 package com.vignan.tracker
 
 import android.os.Build
+import java.util.Calendar
 
 class AndroidPlatform : Platform {
     override val name: String = "Android ${Build.VERSION.SDK_INT}"
@@ -8,3 +9,10 @@ class AndroidPlatform : Platform {
 }
 
 actual fun getPlatform(): Platform = AndroidPlatform()
+
+actual fun getCurrentTimeInfo(): TimeInfo {
+    val cal = Calendar.getInstance()
+    val dayIdx = cal.get(Calendar.DAY_OF_WEEK) - 1 // 0=Sun..6=Sat
+    val mins = cal.get(Calendar.HOUR_OF_DAY) * 60 + cal.get(Calendar.MINUTE)
+    return TimeInfo(dayIdx, mins)
+}
--- shared/src/commonMain/kotlin/com/vignan/tracker/Platform.kt
+++ shared/src/commonMain/kotlin/com/vignan/tracker/Platform.kt
@@ -5,4 +5,10 @@ interface Platform {
     val appVersion: String
 }
 
+data class TimeInfo(
+    val dayOfWeekIdx: Int, // 0 = Sun, 1 = Mon ... 6 = Sat
+    val minutesOfDay: Int   // hours * 60 + minutes
+)
+
 expect fun getPlatform(): Platform
+expect fun getCurrentTimeInfo(): TimeInfo
